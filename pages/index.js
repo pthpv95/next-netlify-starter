60d86cb2c7d9c4a745ebbd9e4a4efe01595f1b07
The Hien <thehienpv@gmail.com>
2021-01-02 09:33:35 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+      <h2>Hello Hien Pham</h2>
       </main>
 
       <Footer />
